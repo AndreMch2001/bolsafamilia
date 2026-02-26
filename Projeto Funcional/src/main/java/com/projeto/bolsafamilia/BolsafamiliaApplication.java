406a8a9a3fd5d6b04c52c1c3938771ad6e7221eb
jonas Tillmann Junior <jonastillmann19@gmail.com>
2026-02-25 21:41:22 -0300

    Criação da API base para requisições sem JPA - branch Desenvolvimento

---
+++ Projeto Funcional/src/main/java/com/projeto/bolsafamilia/BolsafamiliaApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.bolsafamilia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolsafamiliaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BolsafamiliaApplication.class, args);
+	}
+
+}
